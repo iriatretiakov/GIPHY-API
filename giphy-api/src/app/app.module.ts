82f24fae29b068536cce1635cccf343a37e0a95e
ilyat <ilya.tretyakov@velaskec.com>
2022-10-15 18:55:14 +0900

    add content, header, search components and data service

--- giphy-api/src/app/app.module.ts
+++ giphy-api/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { ContentComponent } from './content/content.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
